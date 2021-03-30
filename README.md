5b4d50c501bb7c1ec746c470f690b231e5d9412b
Bushycargo <josephaines03@gmail.com>
2021-03-30 15:45:29 +0100

    Added new flag and updated README

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Installs my customization for any linux distro using pacman.
 Once install is complete log out then log back in with i3wm.
 
 Flags:
- -s : The flag will append to the end of the .bashrc file to add my scripts to the PATH. 
+ -b : This flag will append to the end of the .bashrc file to add my scripts to the PATH. 
+ -s : This flag will not fully install but will update the scripts folder.
 # linux-files
--- install.sh
+++ install.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
 echo Starting Install
+
+while getopts ":b:s" opt; do
+	case $opt in
+		b)
+			cat bashrcAppend >> ~/.bashrc
+			echo "Appended .bashrc" >&2
+		;;
+		s)
+			cp -r scripts/* ~/
+			echo "Updated Scripts. Install Finished" >&2
+			exit 0
+		;;
+		\?)
+			echo "Error invalid flag: -$OPTARG" >&2
+			exit 0
+		;;
+	esac
+done
+
 sudo pacman -S feh compton rofi i3-wm i3lock i3status
+
 mkdir ~/.config/i3 || true
 mkdir ~/scripts || true
+
 cp -i config ~/.config/i3/
 cp background.jgp ~/.config/i3/
 cp background.png ~/.config.i3/
 cp -r scripts/* ~/
+
 chmod +x ~/scripts/*
 
-while getopts ":s" opt; do
-	case $opt in
-		s)
-			cat bashrcAppend >> ~/.bashrc
-			echo "Appended .bashrc" >&2
-		;;
-		\?)
-			echo "Invalid flag: -$OPTARG" >&2
-		;;
-	esac
-done
 echo Install Done!
